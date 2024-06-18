67e451a152a1fc9b97a6c21cd44063a499410f3d
QuentinHamak <127999143+QuentinHamak@users.noreply.github.com>
2024-06-17 22:28:27 -0400

    Add buttons/tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,16 +1,23 @@
+import ButtonTab from "./ButtonTab";
 import CodeTab from "./CodeTab"
+import { useSelector } from "react-redux"
+import { useState } from "react";
 
 export default function Tabs() {
-  return (
-    <div className="flex grow">
-        <div className="flex grow flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
-            <button>HTML</button>
-            <button>CSS</button>
-            <button>JS</button>
+
+    const tabsState = useSelector(state => state.tabs)
+    const [tabIndex, setTabIndex] = useState(tabsState[0].id)
+
+    return (
+        <div className="flex grow">
+            <div className="flex grow flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
+                {tabsState.map(tab => (
+                    <ButtonTab key={tab.id} id={tab.id} toggleTab={id => setTabIndex(id)} imgURL={tab.imgURL} buttonContent={tab.buttonContent} />
+                ))}
+            </div>
+            <div className="w-full grow relative">
+                <CodeTab id={tabIndex} code={tabsState.find(obj => obj.id === tabIndex).code} />
+            </div>
         </div>
-        <div className="w-full grow relative">
-            <CodeTab />
-        </div>
-    </div>
-  )
+    )
 }
